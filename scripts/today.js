6487a2367e782a94bb70a134f7d382437dda3ea0
Ryan Christy <call900913@icloud.com>
2022-03-16 03:15:46 -0400

    Refactor code

--- scripts/today.js
+++ scripts/today.js
@@ -2,3 +2,7 @@ function day(date) {
   const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   return weekDays[date.getDay()];
 }
+
+function greet(time) {
+  return `Hello! Have a good ${time}!`
+}
